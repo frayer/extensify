4303dd01044bdb4832c7fca198f4254913183872
frayer <mrfrayer@yahoo.com>
2011-01-09 15:21:34 -0500

    Adding implementation of ClosureExtensibleXalanTransformer as well as tests.

---
+++ transform-extensions-groovy/src/main/java/org/extensify/transform/xalan/extensions/groovy/ClosureExtensibleXalanTransformer.java
@@ -0,0 +1,27 @@
+package org.extensify.transform.xalan.extensions.groovy;
+
+import groovy.lang.Closure;
+import org.extensify.transform.xalan.transformer.ExtensibleXalanTransformer;
+import org.apache.xalan.transformer.TransformerImpl;
+
+class ClosureExtensibleXalanTransformer extends ExtensibleXalanTransformer {
+
+    public ClosureExtensibleXalanTransformer(TransformerImpl baseTransformer) {
+        super(baseTransformer);
+    }
+
+    public void addExtensionFunction(String namespaceUri, String name, Closure closure) {
+        GroovyClosureExtensionHandler handler = new GroovyClosureExtensionHandler(namespaceUri);
+        handler.addFunctionClosure(name, closure);
+
+        this.registerExtensionHandler(namespaceUri, handler);
+    }
+
+    public void addExtensionElement(String namespaceUri, String name, Closure closure) {
+        GroovyClosureExtensionHandler handler = new GroovyClosureExtensionHandler(namespaceUri);
+        handler.addElementClosure(name, closure);
+
+        this.registerExtensionHandler(namespaceUri, handler);
+    }
+
+}
---
+++ transform-extensions-groovy/src/test/groovy/org/extensify/transform/xalan/extensions/groovy/ClosureExtensibleXalanTransformerTest.groovy
@@ -0,0 +1,58 @@
+package org.extensify.transform.xalan.extensions.groovy
+
+import javax.xml.transform.Source
+import javax.xml.transform.TransformerFactory
+import javax.xml.transform.stream.StreamResult
+import javax.xml.transform.stream.StreamSource
+import org.apache.commons.io.IOUtils
+import org.apache.xalan.templates.ElemTemplateElement
+import org.custommonkey.xmlunit.XMLAssert
+import org.junit.Before
+import org.junit.Test
+
+class ClosureExtensibleXalanTransformerTest {
+
+  private static final String EXTENSION_TEST_NS = "http://www.xsltextensions.org/extensions"
+  private static final String XSL_PATH = "/org/extensify/transform/xalan/extensions/groovy/people_xform.xsl"
+  private static final String XML_PATH = "/org/extensify/transform/xalan/extensions/groovy/people.xml"
+  private static final String EXPECTED_XML_PATH = "/org/extensify/transform/xalan/extensions/groovy/people_expected.xml"
+
+  ClosureExtensibleXalanTransformer closureTransformer = null
+  Source xmlSource = null
+  Source xslSource = null
+
+  @Before
+  void setUp() {
+    xmlSource = new StreamSource(ClosureExtensibleXalanTransformerTest.class.getResourceAsStream(XML_PATH))
+    xslSource = new StreamSource(ClosureExtensibleXalanTransformerTest.class.getResourceAsStream(XSL_PATH))
+
+    def transformer = TransformerFactory.newInstance().newTransformer(xslSource)
+    closureTransformer = new ClosureExtensibleXalanTransformer(transformer)
+  }
+
+  @Test
+  void testNothing() {
+    def variables = [:]
+
+    closureTransformer.addExtensionFunction(EXTENSION_TEST_NS, "variable") { context, name ->
+      variables[name]
+    }
+
+    closureTransformer.addExtensionElement(EXTENSION_TEST_NS, "set-variable") { ElemTemplateElement elemTemplateElement, transformer, stylesheet ->
+      def name = elemTemplateElement.getAttribute('name')
+      def value = elemTemplateElement.getAttribute('value')
+      variables[name] = value
+    }
+
+    ByteArrayOutputStream outputStream = new ByteArrayOutputStream()
+    StreamResult result = new StreamResult(outputStream)
+
+    closureTransformer.transform(xmlSource, result)
+
+    def expectedXML = IOUtils.toString(ClosureExtensibleXalanTransformerTest.class.getResourceAsStream(EXPECTED_XML_PATH))
+    def transformedXML = outputStream.toString()
+
+    XMLAssert.assertXMLEqual(expectedXML, transformedXML)
+  }
+
+  }
